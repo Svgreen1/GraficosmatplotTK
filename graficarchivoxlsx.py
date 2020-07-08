135781f938ebec00aef0179d316c93918a686d15
Svgreen1 <66741575+Svgreen1@users.noreply.github.com>
2020-07-08 11:28:05 -0500

    Update graficarchivoxlsx.py

--- graficarchivoxlsx.py
+++ graficarchivoxlsx.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-libro = "C:\\Users\\Sebastian_Valverde\\Desktop\\Excel tesis\\servi.xlsx"
+libro = "C:\\Users\\Sebastian_Valverde\\Desktop\\Excel\\servi.xlsx"
 
 df= pd.read_excel(libro, header=0, delim_whitespace=True)
 #print(df.head())
